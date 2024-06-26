1027954e6114ff4809979c0d991aa473fbe59eb1
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-06-26 14:54:37 +0100

    Created produce the data for each team
    Running through each team and processing the data so the user has the name, points, wins, loses, draws and total number of goals scored for each team.

--- main.go
+++ main.go
@@ -22,13 +22,22 @@ type Match struct {
 	FullTimeAwayGoals int    `json:"fulltimeawaygoals"`
 }
 
-// type Teams struct {
-// 	Teams []Team
-// }
+type UnfilteredTeams struct {
+	Teams []string
+}
 
-// type Team struct {
-// 	Name string
-// }
+type Teams struct {
+	Teams []Team
+}
+
+type Team struct {
+	Name       string
+	Wins       int
+	Draws      int
+	Loses      int
+	TotalGoals int
+	Points     int
+}
 
 func main() {
 	file, err := os.Open("data.json")
@@ -40,16 +49,63 @@ func main() {
 	byteValue, _ := io.ReadAll(file)
 
 	var matches Matches
-	var teams []string
+	var unfilteredTeams UnfilteredTeams
+	var teams Teams
+	// var teamsList []string
 
 	json.Unmarshal(byteValue, &matches)
 
 	for i := 0; i < len(matches.Matches); i++ {
 		// fmt.Printf("| Match Date: %s | Season Game: %d | Match Day: %d | Full Time Result: %s | Home Team: %s | Full Time Home Goals: %d | Away Team: %s | Full Time Away Goals: %d |\n", matches.Matches[i].MatchDate, matches.Matches[i].SeasonGame, matches.Matches[i].MatchDay, matches.Matches[i].FullTimeResult, matches.Matches[i].HomeTeam, matches.Matches[i].FullTimeHomeGoals, matches.Matches[i].AwayTeam, matches.Matches[i].FullTimeAwayGoals)
-		teams = append(teams, matches.Matches[i].HomeTeam, matches.Matches[i].AwayTeam)
+		unfilteredTeams.Teams = append(unfilteredTeams.Teams, matches.Matches[i].HomeTeam, matches.Matches[i].AwayTeam)
 	}
 
-	fmt.Println(removeDuplicates(teams))
+	unfilteredTeams.Teams = removeDuplicates(unfilteredTeams.Teams)
+
+	for i := 0; i < len(unfilteredTeams.Teams); i++ {
+		var team Team
+
+		team.Name = unfilteredTeams.Teams[i]
+
+		for i := 0; i < len(matches.Matches); i++ {
+			if matches.Matches[i].HomeTeam != team.Name && matches.Matches[i].AwayTeam != team.Name {
+				continue
+			}
+
+			if matches.Matches[i].HomeTeam != team.Name {
+				switch matches.Matches[i].FullTimeResult {
+				case "H":
+					team.Wins++
+					team.Points += 3
+				case "A":
+					team.Loses++
+				case "D":
+					team.Draws++
+					team.Points++
+				}
+
+				team.TotalGoals += matches.Matches[i].FullTimeHomeGoals
+			} else {
+				// If the above isn't true, we know the team is either home or away thus must be away
+				switch matches.Matches[i].FullTimeResult {
+				case "H":
+					team.Loses++
+				case "A":
+					team.Wins++
+					team.Points += 3
+				case "D":
+					team.Draws++
+					team.Points++
+				}
+
+				team.TotalGoals += matches.Matches[i].FullTimeAwayGoals
+			}
+		}
+		fmt.Printf("Name: %s, Points: %d, Wins: %d, Loses: %d, Draws: %d, Goals: %d,\n", team.Name, team.Points, team.Wins, team.Loses, team.Draws, team.TotalGoals)
+		teams.Teams = append(teams.Teams, team)
+	}
+
+	// fmt.Println(removeDuplicates(unfilteredTeams.Teams))
 
 	// fmt.Print(teams)
 
